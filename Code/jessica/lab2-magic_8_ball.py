ffd6c6f04824a2b14b1c7ad299e3853005ba0d4b
Jessica <jlpavlinko@gmail.com>
2017-07-24 21:22:09 -0700

    add lab 1 and 2

---
+++ Code/jessica/lab2-magic_8_ball.py
@@ -0,0 +1,11 @@
+output = 'Welcome!'
+print (output)
+
+input('Ask the 8-ball a question:')
+
+
+import random
+
+predictions = ['Yes', 'It is decidedly so', 'No way!', 'All signs point to yes', 'Outlook not so good']
+prediction = random.choice(predictions)
+print (prediction)
