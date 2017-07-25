ac0075a09636f6069f11f6f16e019213be29f174
tristaramirez <missramirez.tr@gmail.com>
2017-07-25 11:31:57 -0700

    Added lab02 8 ball updated

--- Code/trista/Lab-02 randon.choice.py
+++ Code/trista/Lab-02 randon.choice.py
@@ -1,9 +1,9 @@
 print ('Welcome to Lab02')
 print('\v')  #space between lines
-question = input ('Ask the Magic 8-Ball anything:') #user will ask a random question
+question = input ('Ask the Magic 8-Ball anything:') # User will ask a random question
 
 import random
 
-answers = ['Yes!', 'Not really', 'I think not', 'Absolutely', 'Maybe so'] #python will choose an answer from the brackets
+answers = ['Yes!', 'Not really', 'I think not', 'Absolutely', 'Maybe so'] # Python will choose an answer from the brackets
 answer = random.choice(answers)
 print(answer)
