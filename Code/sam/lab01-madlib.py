b26f64e5c3e518dbdbfb66bc273bb86e3e983b2c
smoad <samanthagmoad@gmail.com>
2017-07-24 19:59:41 -0700

    Lab01 madlibs exercise.

---
+++ Code/sam/lab01-madlib.py
@@ -0,0 +1,8 @@
+nop = input('Choose a name of a person. ')
+place = input('Choose a name of a place. ')
+ptv = input('Choose a past-tense verb. ')
+noun = input('Choose a noun. ')
+
+output= ' One day, ' + nop + ' went to ' + place + ' and ' + ptv + ' your ' + noun
+
+print(output)
