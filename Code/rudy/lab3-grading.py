e94df821d0e0f1c16839d682b3303e73681a80c4
IcarusSprite <rudolphvdesign@gmail.com>
2017-07-25 19:09:24 -0700

    Rudy lab3 to repository.

--- Code/rudy/lab3-grading.py
+++ Code/rudy/lab3-grading.py
@@ -1,18 +1,24 @@
-score = input('Please input your score (0-100) here ')
 
-x = score
-x = int(x)
-if x >100:
-    print('Sorry. That is not a valid score. You failed, try again.')
-elif x > 90:
-    print('You scored an A')
-elif x > 80:
-    print('You scored an B')
-elif x > 70:
-    print('You scored an C')
-elif x > 60:
-    print('You scored an D')
-else:
-    print('You scored an F')
+
+while True: # while is a loop    true is a condition of the loop.
+    score = input('Please input your score (0-100) here ')
+
+    x = score
+    x = int(x)
+    if x > 100:
+        print('Sorry. That is not a valid score. You failed, try again.')
+        continue # continue signals that the loop will restart to beginning
+    elif x > 90:
+        print('You scored an A')
+    elif x > 80:
+        print('You scored an B')
+    elif x > 70:
+        print('You scored an C')
+    elif x > 60:
+        print('You scored an D')
+    else:
+        print('You scored an F')
+
+    break # break signals that the loop is complete and to exit loop
 
 
