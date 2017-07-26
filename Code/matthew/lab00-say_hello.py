4022f7e968e22520b19f528c10a4c7baffbc2c01
Matthew Cooper <simian201@gmail.com>
2017-07-25 18:22:02 -0700

    added lab 3 and my solutions

---
+++ Code/matthew/lab00-say_hello.py
@@ -0,0 +1,8 @@
+"""
+this is a simple program which asks the user their name, then says 'hello'
+it uses input and print
+"""
+
+name = input('what is your name? ')
+output = 'hello, ' + name + '!'
+print(output)
---
+++ Code/matthew/lab03-grading.py
@@ -0,0 +1,12 @@
+
+x = 5
+if x < 2:
+    print('x is less than 2')
+elif x < 10:
+    print('x is between 2 and 10')
+else:
+    print('x is greater or equal to 10')
+
+
+
+
