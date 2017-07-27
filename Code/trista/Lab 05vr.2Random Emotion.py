d95ce1422509af1ca21f034ce942e5ccbba41d3d
tristaramirez <missramirez.tr@gmail.com>
2017-07-26 19:09:26 -0700

    Added lab05 Version 2 added with a new folder

---
+++ Code/trista/Lab 05vr.2Random Emotion.py
@@ -0,0 +1,10 @@
+
+print('Random Emotion Generator:')
+
+import random
+
+
+
+emotions = ['--> Happy', '--> Mad', '--> Stressed', '--> Calm', '--> Excited']
+emotions = random.choice(emotions)
+print(emotions)
