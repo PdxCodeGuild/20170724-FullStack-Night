d7fefbd8d3b86afb9f32d24c9bd04a2f3bb226ae
Jessica <jlpavlinko@gmail.com>
2017-07-31 21:37:14 -0700

    add lab 11.3

---
+++ Code/jessica/lab11-unit_converter_v.3.py
@@ -0,0 +1,17 @@
+while True:
+    distance = float(input('What is the distance? '))
+    units = input('What are the units? ')
+    if distance == 'done':
+        break
+    if units == 'foot' or units == 'feet':
+        print(str(distance) + ' ' + units + ' is ' + str(distance * 0.3048) + ' meters')
+    elif units == 'mile' or units == 'miles':
+        print(str(distance) + ' ' + units + ' is ' + str(distance * 1609.34) + ' meters')
+    elif units == 'meter' or units == 'meters':
+        print(str(distance) + ' ' + units + ' is ' + str(distance) + ' meters')
+    elif units == 'kilometer' or units == 'kilometers':
+        print(str(distance) + ' ' + units + ' is ' + str(distance * 1000) + ' meters')
+    elif units == 'yard' or units == 'yards':
+        print(str(distance) + ' ' + units + ' is ' + str(distance * .9144) + ' meters')
+    elif units == 'inch' or units == 'inches':
+        print(str(distance) + ' ' + units + ' is ' + str(distance * .0254) + ' meters')
