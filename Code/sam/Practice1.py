4dea2c1ba232e534571fbd1b84da5792e4408b98
smoad <samanthagmoad@gmail.com>
2017-08-03 21:32:03 -0700

    Practice problem 1: write a function that tells whether a number is even or odd.

--- Code/sam/Practice1.py
+++ Code/sam/Practice1.py
@@ -1,3 +1,14 @@
-user_input = input('What is your number: ')
 
-def even()
+def even_or_odd(user_input):
+    if user_input / 2 == user_input // 2:
+        return 'even'
+    else:
+        return 'odd'
+
+def main():
+    user_input = int(input('What is your number: '))
+    answer = even_or_odd(user_input)
+    print(answer)
+
+
+main()
