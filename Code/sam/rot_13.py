50cadcc4db0bcecd799d627a3bbab133a712bd7f
smoad <samanthagmoad@gmail.com>
2017-08-07 18:05:47 -0700

    Rot13 exercise.

--- Code/sam/rot_13.py
+++ Code/sam/rot_13.py
@@ -1,20 +1,20 @@
 
 
-def rot13(user_string):                                      # defining the function for rot13
-    encrypted = ''
+def rot13(user_string):                              # defining the function for rot13
+    encrypted = ''                                   # creating a blank str
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     alphabet_rotated = 'nopqrstuvwxyzabcdefghijklm'
-    for char in user_string:                                  # creating a for loop
-        char_index = alphabet.find(char)                      # finding the index of element in alphabet string
-        rotated_char = alphabet_rotated[char_index]
-        encrypted = encrypted + rotated_char
+    for char in user_string:
+        char_index = alphabet.find(char)             # finding index of element in alphabet str
+        rotated_char = alphabet_rotated[char_index]  # using char_index to find element in a_r
+        encrypted = encrypted + rotated_char         # adding element to empty str (same as +=)
 
-    return encrypted
+    return encrypted                                 # returning encrypted to function
 
 
 def main():
     user_string = input('Type the message you want encrypted: ')
-    print('Here is your encrypted message: ' + rot13(user_string))
+    print('Here is your encrypted message: ' + rot13(user_string))  # string concatenation
 
 
-main()
+main()                                               # calling main function
