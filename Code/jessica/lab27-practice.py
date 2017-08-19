1f3c8ae8b5f3e114c362c0b82711d4540a0d280a
Jessica <jlpavlinko@gmail.com>
2017-08-19 15:24:07 -0700

    add lab 27.1 updated

--- Code/jessica/lab27-practice.py
+++ Code/jessica/lab27-practice.py
@@ -1,8 +1,8 @@
 # Write a function that takes two ints, a and b, and returns True if one is positive and the other is negative.
 def opposite(a, b):
-    if a < 0 != b > 0:  # instead of a < 0 and b > 0
+    if a < 0 and b > 0:
         return True
-    elif a > 0 != b < 0:  # instead of a > 0 and b < 0
+    elif a > 0 and b < 0:
         return True
     else:
         return False
